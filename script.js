1f393ec58b2b0b62a8ac17e5524da8474af95697
Aditya Batra <adityabatra@Adityas-Air.Dlink>
2017-07-29 03:21:04 +0530

    navbar

--- script.js
+++ script.js
@@ -5,4 +5,15 @@ $(document).ready(function(){
 			scrollTop:$('.video-div').offset().top
 		},1000);
 	});
+	
+	  var toggle = 0;
+  $("#nav-img").click(function() {
+    if (!toggle) {
+      $("#full-nav").css({"display":"block"});
+      toggle = 1;
+    } else {
+      $("#full-nav").css({"display":"none"});
+      toggle = 0;
+    }
+  });
 });
